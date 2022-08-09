edebd46123d427b215370f19e59369b5923fe262
Benny623 <bmilner88@gmail.com>
2022-08-08 21:45:39 -0700

    updated link in server.js for app.use

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
