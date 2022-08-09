b792a973271b1f9dd9133cc6c5d72d6763974848
Benny623 <bmilner88@gmail.com>
2022-08-08 21:52:42 -0700

    updated sendfile in server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, './client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
