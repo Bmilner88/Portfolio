484c692a0965c0f1f41b3098973343ba08327826
Ben Milner <bmilner88@gmail.com>
2022-08-22 18:20:24 -0700

    Updated portfolio items
    Added Skyscape to the item list and removed weather dashboard.

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -8,6 +8,24 @@ export default function Portfolio() {
         <h2>Portfolio</h2>
         <div className="row">
           <Fade left>
+            <div className="col-sm-6 pb-3">
+              <div className="project-caption m-3">
+                <h3 className="text-center">Skyscape</h3>
+                <a
+                  href="https://bm-skyscape.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={require(`../../assets/images/portfolio/skyscape.png`)}
+                    className="img-fluid rounded"
+                    alt="Skyscape website"
+                  ></img>
+                </a>
+              </div>
+            </div>
+          </Fade>
+          <Fade right>
             <div className="col-sm-6">
               <div className="project-caption m-3">
                 <h3 className="text-center">Regal</h3>
@@ -17,70 +35,52 @@ export default function Portfolio() {
                   rel="noreferrer"
                 >
                   <img
-                    src={require(`../../assets/images/regal.png`)}
+                    src={require(`../../assets/images/portfolio/regal.png`)}
                     className="img-fluid rounded"
-                    alt="Regal Website"
+                    alt="Regal website"
                   ></img>
                 </a>
               </div>
             </div>
           </Fade>
-          <Fade right>
-            <div className="col-sm-6">
-              <div className="project-caption m-3">
-                <h3 className="text-center">Budgetek</h3>
-                <a
-                  href="https://quiet-tundra-30236.herokuapp.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src={require(`../../assets/images//budgetek.png`)}
-                    className="img-fluid rounded"
-                    alt="Regal Website"
-                  ></img>
-                </a>
+          <div className="row">
+            <Fade left>
+              <div className="col-sm-6">
+                <div className="project-caption m-3">
+                  <h3 className="text-center">Budgetek</h3>
+                  <a
+                    href="https://quiet-tundra-30236.herokuapp.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={require(`../../assets/images/portfolio/budgetek.png`)}
+                      className="img-fluid rounded"
+                      alt="Budgetek website"
+                    ></img>
+                  </a>
+                </div>
               </div>
-            </div>
-          </Fade>
-        </div>
-        <div className="row">
-          <Fade left>
-            <div className="col-sm-6">
-              <div className="project-caption m-3">
-                <h3 className="text-center">ShowPro</h3>
-                <a
-                  href="https://BlinkingHeimdall.github.io/show-pro/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src={require(`../../assets/images/showpro.png`)}
-                    className="img-fluid rounded"
-                    alt="Regal Website"
-                  ></img>
-                </a>
+            </Fade>
+            <Fade right>
+              <div className="col-sm-6">
+                <div className="project-caption m-3">
+                  <h3 className="text-center">ShowPro</h3>
+                  <a
+                    href="https://BlinkingHeimdall.github.io/show-pro/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={require(`../../assets/images/portfolio/showpro.png`)}
+                      className="img-fluid rounded"
+                      alt="ShowPro website"
+                    ></img>
+                  </a>
+                </div>
               </div>
-            </div>
-          </Fade>
-          <Fade right>
-            <div className="col-sm-6 pb-3">
-              <div className="project-caption m-3">
-                <h3 className="text-center">Weather Dashboard</h3>
-                <a
-                  href="https://bmilner88.github.io/c6-weather-dashboard/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src={require(`../../assets/images/weatherdashboard.png`)}
-                    className="img-fluid rounded"
-                    alt="Regal Website"
-                  ></img>
-                </a>
-              </div>
-            </div>
-          </Fade>
+            </Fade>
+          </div>
         </div>
       </div>
     </div>
