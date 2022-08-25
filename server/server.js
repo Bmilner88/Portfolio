92dcb485297bda876f18bd653fbe5ef0e90ed55c
Ben Milner <bmilner88@gmail.com>
2022-08-25 16:04:39 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "build")));
 };
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 let transporter = nodemailer.createTransport({
