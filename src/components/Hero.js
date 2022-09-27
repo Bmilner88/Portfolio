6160fc3ef0f25a5f9b6cec10ac9c5274b7973863
Ben Milner <bmilner88@gmail.com>
2022-09-27 08:57:40 -0700

    Removed CSS styling and component content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,40 +1,11 @@
 import React from "react";
-import { Fade } from "react-awesome-reveal";
 
-export default function Hero() {
+const Hero = () => {
   return (
-    <div className="my-5 pb-5">
-      <Fade>
-        <div className="container">
-          <div style={{ minHeight: "75vh" }} className="row justify-content-center align-items-center g-5">
-            <div className="col-sm-5">
-              <img
-                className="img-fluid rounded"
-                src={require("../assets/images/main/ben-milner.jpg")}
-                alt="Ben Milner"
-              />
-            </div>
-            <div className="col-sm-1"></div>
-            <div className="col-sm">
-              <p className="m-0 about-text">
-                Hi there! My name is Ben Milner, and I am seeking employment in
-                the field of Web Development. I am well qualified, having
-                completed a Full-Stack Web Development Boot Camp through the
-                University of Utah in addition to my professional and personal
-                experience. I am a fast learner, a reliable employee, and I work
-                well with others both as part of a team and in assisting clients
-                and fellow employees.
-                <br />
-                <br />I have about 4 years of professional experience in IT and
-                have always been fascinated with technology. In my spare time I
-                enjoy skiing, hiking and playing lots of games. I've been
-                learning how to resin 3D print which definitely keeps my mind
-                active. I love traveling to new places and learning new things.
-              </p>
-            </div>
-          </div>
-        </div>
-      </Fade>
-    </div>
+    <>
+
+    </>
   );
 }
+
+export default Hero;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,96 +1,11 @@
 import React from "react";
-import { Fade } from "react-awesome-reveal";
 
-export default function Portfolio() {
+const Portfolio = () => {
   return (
     <>
-      <div className="container-fluid">
-        <h2>Portfolio</h2>
-        <div className="row">
-          <div className="col-sm pb-3">
-            <Fade direction="left">
-              <div className="project-caption m-3">
-                <h3 className="text-center">Skyscape</h3>
-                <a
-                  href="https://bm-skyscape.onrender.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src={require(`../assets/images/portfolio/skyscape.png`)}
-                    className="img-fluid rounded"
-                    alt="Skyscape website"
-                  ></img>
-                </a>
-              </div>
-            </Fade>
-          </div>
-          <div className="col-sm">
-            <Fade direction="right">
-              <div className="project-caption m-3">
-                <h3 className="text-center">Regal</h3>
-                <a
-                  href="https://regal-help.herokuapp.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src={require(`../assets/images/portfolio/regal.png`)}
-                    className="img-fluid rounded"
-                    alt="Regal website"
-                  ></img>
-                </a>
-              </div>
-            </Fade>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm">
-            <Fade direction="left">
-              <div className="project-caption m-3">
-                <h3 className="text-center">Budgetek</h3>
-                <a
-                  href="https://quiet-tundra-30236.herokuapp.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src={require(`../assets/images/portfolio/budgetek.png`)}
-                    className="img-fluid rounded"
-                    alt="Budgetek website"
-                  ></img>
-                </a>
-              </div>
-            </Fade>
-          </div>
-          <div className="col-sm">
-            <Fade direction="right">
-              <div className="project-caption m-3">
-                <h3 className="text-center">ShowPro</h3>
-                <a
-                  href="https://BlinkingHeimdall.github.io/show-pro/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src={require(`../assets/images/portfolio/showpro.png`)}
-                    className="img-fluid rounded"
-                    alt="ShowPro website"
-                  ></img>
-                </a>
-              </div>
-            </Fade>
-          </div>
-        </div>
-      </div>
-      <a
-        href="https://github.com/Bmilner88?tab=stars"
-        target="_blank"
-        rel="noreferrer"
-        className="link link-dark text-decoration-none"
-      >
-        <button className="btn btn-dark my-3">See More on GitHub</button>
-      </a>
+    
     </>
   );
 }
+
+export default Portfolio;
