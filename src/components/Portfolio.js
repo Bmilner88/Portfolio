5b0baefb1a6b38683774c9e8af8d8f8c4d643646
Ben Milner <bmilner88@gmail.com>
2022-09-28 16:36:41 -0700

    Added Portfolio template

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,9 +2,97 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <section>
-      <div>Tets</div>
-    </section>
+    <>
+      <h2 className="text-center text-slate-300 font-bold text-3xl my-7">Portfolio</h2>
+      <section className="grid grid-cols-2 justify-items-center mb-7 gap-10">
+        <div className="bg-slate-200 rounded-lg w-9/12">
+          <h3 className="text-center text-2xl font-bold my-2">Skyscape</h3>
+          <a
+            href="https://bm-skyscape.onrender.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../assets/images/portfolio/skyscape.png`)}
+              className="rounded-b-lg"
+              alt="Skyscape website"
+            ></img>
+          </a>
+        </div>
+        <div className="bg-slate-200 rounded-lg w-9/12">
+          <h3 className="text-center text-2xl font-bold my-2">Skyscape</h3>
+          <a
+            href="https://bm-skyscape.onrender.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../assets/images/portfolio/skyscape.png`)}
+              className="rounded-b-lg"
+              alt="Skyscape website"
+            ></img>
+          </a>
+        </div>
+
+        <div className="bg-slate-200 rounded-lg w-9/12">
+          <h3 className="text-center text-2xl font-bold my-2">Skyscape</h3>
+          <a
+            href="https://bm-skyscape.onrender.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../assets/images/portfolio/skyscape.png`)}
+              className="rounded-b-lg"
+              alt="Skyscape website"
+            ></img>
+          </a>
+        </div>
+        <div className="bg-slate-200 rounded-lg w-9/12">
+          <h3 className="text-center text-2xl font-bold my-2">Skyscape</h3>
+          <a
+            href="https://bm-skyscape.onrender.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../assets/images/portfolio/skyscape.png`)}
+              className="rounded-b-lg"
+              alt="Skyscape website"
+            ></img>
+          </a>
+        </div>
+
+        <div className="bg-slate-200 rounded-lg w-9/12">
+          <h3 className="text-center text-2xl font-bold my-2">Skyscape</h3>
+          <a
+            href="https://bm-skyscape.onrender.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../assets/images/portfolio/skyscape.png`)}
+              className="rounded-b-lg"
+              alt="Skyscape website"
+            ></img>
+          </a>
+        </div>
+        <div className="bg-slate-200 rounded-lg w-9/12">
+          <h3 className="text-center text-2xl font-bold my-2">Skyscape</h3>
+          <a
+            href="https://bm-skyscape.onrender.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../assets/images/portfolio/skyscape.png`)}
+              className="rounded-b-lg"
+              alt="Skyscape website"
+            ></img>
+          </a>
+        </div>
+      </section>
+    </>
   );
 };
 
