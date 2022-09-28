adee9fbb7d3cda2bc1ad397e95a80cc3db088071
Ben Milner <bmilner88@gmail.com>
2022-09-28 16:25:10 -0700

    Couple of changes --
    - Removed unnecessary npm packages
    - Added footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { BsLinkedin, BsGithub, BsStackOverflow } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer>
-
-    </footer>
+    <div className="grid grid-cols-3 items-center py-3">
+      <a
+        href="https://www.linkedin.com/in/bmilner88"
+        target="_blank"
+        rel="noreferrer"
+        className="text-white justify-self-end"
+      >
+        <span>
+          <BsLinkedin size={25} />
+        </span>
+      </a>
+      <a
+        href="https://github.com/Bmilner88"
+        target="_blank"
+        rel="noreferrer"
+        className="text-white justify-self-center"
+      >
+        <span>
+          <BsGithub size={25} />
+        </span>
+      </a>
+      <a
+        href="https://stackoverflow.com/users/19388818/ben-milner"
+        target="_blank"
+        rel="noreferrer"
+        className="text-white justify-self-start"
+      >
+        <span>
+          <BsStackOverflow size={25} />
+        </span>
+      </a>
+    </div>
   );
 };
 
