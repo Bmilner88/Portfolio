27ab58d34bee4a1c6a2177ba75523ee7e4393589
Ben Milner <bmilner88@gmail.com>
2022-09-28 22:41:57 -0700

    Somewhat fixed the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,47 +7,50 @@ const Header = ({ projectsRef }) => {
   const scrollToElement = () => projectsRef.current.scrollIntoView();
 
   return (
-    <header className="fixed top-0 w-screen m-0">
-      {/* 
-      
-      -- NEED TO FOLLOW YT TUT --
-      
-      
-      <nav className="bg-gradient-to-r from-green-400 to-blue-400 shadow-lg">
-        <div className="container mx-auto">
-          <h1 className="text-white text-3xl font-bold p-3 inline-flex">
-            Ben Milner
-          </h1>
-          <button className="text-white inline-flex justify-center transition duration-500 rounded-full hover:text-slate-700 hover:bg-[#BFbada55] lg:hidden">
-            <BiMenu size={25} />
-          </button>
-          <div>
-            <ul className="text-white sm:self-center text-xl border-t sm:border-none">
-              <li className="sm:inline-block">
-                <button
-                  className="p-3 transition duration-500 rounded-xl hover:text-slate-700 hover:bg-[#BFbada55]"
-                  onClick={scrollToElement}
-                >
-                  Projects
-                </button>
-              </li>
-              <li className="sm:inline-block">
-                <a href={pdf} target="_blank" rel="noreferrer">
-                  <button className="sm:hidden p-3 transition duration-500 rounded-md hover:text-slate-700 hover:bg-[#BFbada55]">
-                    Resume
-                  </button>
-                </a>
-              </li>
-              <li className="sm:inline-block">
-                <button className="p-3 transition duration-500 rounded-xl hover:text-slate-700 hover:bg-[#BFbada55]">
-                  Contact
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> */}
-    </header>
+    <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-green-400 to-blue-400 shadow-lg p-6 fixed w-screen z-10 top-0">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <h1 className="text-white text-3xl font-bold">Ben Milner</h1>
+      </div>
+
+      <div className="block sm:hidden">
+        <button
+          onClick={() => {
+            document.getElementById("nav-content").classList.toggle("hidden");
+          }}
+          className="flex items-center px-3 py-2 rounded text-white hover:text-slate-700 hover:bg-[#BFbada55]"
+        >
+          <BiMenu size={25} />
+        </button>
+      </div>
+
+      <div
+        id="nav-content"
+        className="w-full flex-grow sm:flex sm:items-center sm:w-auto hidden sm:block pt-6 sm:pt-0"
+      >
+        <ul className="list-reset sm:flex justify-end flex-1 items-center">
+          <li className="mr-3">
+            <button
+              className="text-white font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700 hover:bg-[#BFbada55]"
+              onClick={scrollToElement}
+            >
+              Projects
+            </button>
+          </li>
+          <li className="mr-3">
+            <a href={pdf} target="_blank" rel="noreferrer">
+              <button className="text-white font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700 hover:bg-[#BFbada55]">
+                Resume
+              </button>
+            </a>
+          </li>
+          <li className="mr-3">
+            <button className="text-white font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700 hover:bg-[#BFbada55]">
+              Contact
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
