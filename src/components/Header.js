190136defb58b5c168c390d0a60b702917e449d8
Ben Milner <bmilner88@gmail.com>
2022-09-29 14:17:18 -0700

    Added form stuff to modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,81 @@ const Header = ({ projectsRef }) => {
 
   const [showModal, setShowModal] = useState(false);
 
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [message, setMessage] = useState({
+    set: false,
+    text: "",
+    loading: false,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formState.name || !formState.email || !formState.message) {
+      setMessage({
+        sent: true,
+        text: "Please fill in all fields!",
+      });
+      return;
+    }
+
+    setMessage({
+      sent: true,
+      text: "Sending",
+      loading: true,
+    });
+
+    await fetch("https://bm-portfolio-api.onrender.com/api/send/", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(formState),
+    })
+      .then((res) => res.json())
+      .then(async (res) => {
+        const resData = await res;
+        if (resData.status === "success") {
+          setMessage({
+            sent: true,
+            text: "Message Sent!",
+            loading: false,
+          });
+
+          setFormState({
+            name: "",
+            email: "",
+            message: "",
+          });
+        } else if (resData.status === "fail") {
+          setMessage({
+            sent: true,
+            text: `Message failed to send - Code: ${res.message.code}`,
+            loading: false,
+          });
+        }
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setMessage({
+      sent: false,
+      text: "",
+    });
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-green-400 to-blue-400 shadow-lg p-6 fixed w-screen z-10 top-0">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -46,7 +121,10 @@ const Header = ({ projectsRef }) => {
             </a>
           </li>
           <li className="mr-3">
-            <button onClick={() => setShowModal(true)} className="text-white font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700 hover:bg-[#BFbada55]">
+            <button
+              onClick={() => setShowModal(true)}
+              className="text-white font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700 hover:bg-[#BFbada55]"
+            >
               Contact
             </button>
           </li>
@@ -58,10 +136,8 @@ const Header = ({ projectsRef }) => {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Modal Title</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -72,9 +148,65 @@ const Header = ({ projectsRef }) => {
                   </button>
                 </div>
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    Form Goes Here
-                  </p>
+                  <form className="container">
+                    <div className="row justify-content-center mx-auto">
+                      <div className="form-group">
+                        <label for="name">Name</label>
+                        <input
+                          className="form-control"
+                          placeholder="John Doe"
+                          name="name"
+                          type="text"
+                          value={formState.name}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label className="mt-3" for="email">
+                          Email
+                        </label>
+                        <input
+                          className="form-control"
+                          placeholder="name@example.com"
+                          name="email"
+                          type="email"
+                          value={formState.email}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label className="mt-3" for="message">
+                          Message
+                        </label>
+                        <textarea
+                          className="form-control"
+                          name="message"
+                          type="text"
+                          rows="8"
+                          value={formState.message}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      {message.sent && message.loading && (
+                        <div>
+                          <div id="message-sent" className="mt-4">
+                            <p>
+                              {message.text}{" "}
+                              <span
+                                className="spinner-border spinner-border-sm"
+                                role="status"
+                              ></span>
+                            </p>
+                          </div>
+                        </div>
+                      )}
+                      {message.sent && !message.loading && (
+                        <div id="message-sent" className="mt-4">
+                          <p>{message.text}</p>
+                        </div>
+                      )}
+                    </div>
+                  </form>
                 </div>
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
@@ -87,7 +219,7 @@ const Header = ({ projectsRef }) => {
                   <button
                     className="bg-gradient-to-r from-green-400 to-blue-400 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    onClick={handleSubmit}
                   >
                     Send
                   </button>
