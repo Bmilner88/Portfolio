36c5d730e5d72cdc5de0b724fea5599646b61f40
Ben Milner <bmilner88@gmail.com>
2024-09-06 18:49:45 -0700

    Adjust spacing and add fade animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ module.exports = {
         "lc-river-1": "url('./assets/images/background/lc-river-1.jpg')",
         "lc-river-2": "url('./assets/images/background/lc-river-2.jpg')",
         "lc-river-3": "url('./assets/images/background/lc-river-3.jpg')",
-      }
+      },
+      keyframes: {
+        appear: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        appear: 'appear 2s ease-in-out',
+      },
     },
   },
   plugins: [],
