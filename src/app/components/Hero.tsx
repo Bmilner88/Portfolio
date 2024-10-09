20935d0bdd59e4d205a086e14d97b58fc218935a
Ben Milner <bmilner88@gmail.com>
2024-10-09 16:52:50 -0700

    Moved public folder to proper location

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Hero() {
   return (
@@ -6,11 +6,11 @@ export default function Hero() {
       <div className="section container mx-auto py-40z mt-32">
         <div className="columns-2">
           <Image
-            className="gap-8 w-1/2 rounded-lg bg-gradient-to-br from-teal-400 to-blue-400 p-1 shadow-xl shadow-slate-900"
             src="/images/main/ben-milner.jpg"
             width={500}
             height={500}
             alt="Ben Milner"
+            className="gap-8 w-1/2 rounded-lg bg-gradient-to-br from-teal-400 to-blue-400 p-1 shadow-xl shadow-slate-900"
           />
         </div>
       </div>
