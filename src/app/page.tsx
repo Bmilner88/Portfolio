16fb99661b3358a9e69f43a6930e7d9a79810ae6
Ben Milner <bmilner88@gmail.com>
2024-10-08 22:09:04 -0700

    Many changes (see desc)
    - Moved public folder to src\app
    - Moved globals.css to app\ui
    - Added Header to app\ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,80 +4,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-slate-800">
-      {/* Header */}
-      <nav className="justify-between flex-wrap p-6 fixed w-screen z-10 top-0">
-        <div
-          id="nav-content"
-          className="w-full flex-grow sm:flex sm:items-center sm:w-auto hidden pt-6 sm:pt-0"
-        >
-          <ul className="list-reset sm:flex justify-center flex-1 items-center">
-            <li>
-              <button className="rounded-full bg-slate-900">
-                Projects
-              </button>
-            </li>
-          </ul>
-          {/* <ul className="list-reset sm:flex justify-center flex-1 items-center">
-            <li className="mr-3">
-              <a
-                href="https://github.com/Bmilner88?tab=stars"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="text-white bg-slate-900 opacity-50 font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700">
-                  Projects
-                </button>
-              </a>
-            </li>
-            <li className="mr-3">
-              <a target="_blank" rel="noreferrer">
-                <button className="text-white font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700">
-                  Resume
-                </button>
-              </a>
-            </li>
-            <li className="mr-3">
-              <button
-                className="text-white font-semibold p-2 transition duration-500 rounded-xl hover:text-slate-700"
-              >
-                Contact
-              </button>
-            </li>
-          </ul> */}
-        </div>
-      </nav>
       {/* Hero */}
-      <section className="pt-28 sm:pt-0 -mt-[88px] animate-appear">
-        <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 lg:mx-60 justify-items-center items-center mt-[88px]">
-          <div className="w-3/4 sm:w-1/2 md:w-9/12">
-            <Image
-              className="rounded-lg shadow-lg self-center transition-all duration-1000 opacity-0 opacity-100"
-              src={"/images/main/ben-milner.jpg"}
-              width={500}
-              height={500}
-              alt="Ben Milner"
-            />
-          </div>
-          <div className="w-11/12 text-justify">
-            <p className="text-slate-300 mt-7 sm:mt-0">
-              Hi there! My name is Ben Milner, and I am seeking employment in
-              the field of Web Development. I am well qualified, having
-              completed a Full-Stack Web Development Boot Camp through the
-              University of Utah in addition to my professional and personal
-              experience. I am a fast learner, a reliable employee, and I work
-              well with others both as part of a team and in assisting clients
-              and fellow employees.
-              <br />
-              <br />I have about 5 years of professional experience in IT and
-              have always been fascinated with technology. In my spare time I
-              enjoy skiing, hiking and playing lots of games. I love traveling
-              to new places and learning new things.
-            </p>
-          </div>
+      <section className="container mx-auto py-40z">
+        <div className="columns-2">
+          <Image
+            className="gap-8 w-1/2 rounded-lg bg-gradient-to-br from-indigo-600 to-pink-600 p-1 shadow-xl shadow-slate-900"
+            src={"/images/main/ben-milner.jpg"}
+            width={500}
+            height={500}
+            alt="Ben Milner"
+          />
         </div>
       </section>
       {/* Portfolio */}
-      <h2 className="text-center text-slate-300 font-bold text-5xl my-12">
+      <h2 id="portfolio" className="text-center text-slate-300 font-bold text-5xl my-12">
         Portfolio
       </h2>
       <section className="h-auto grid grid-cols-1 md:grid-cols-2 justify-items-center mb-7 gap-5">
@@ -181,9 +121,7 @@ export default function Home() {
         Contact
       </h2>
       <section>
-        <div>
-
-        </div>
+        <div></div>
       </section>
       {/* Footer */}
       <div className="flex place-content-center gap-x-10 py-3">
