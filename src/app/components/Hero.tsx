cce61f1b8a674d79d30dbd49f3e8d005dffd1667
Ben Milner <bmilner88@gmail.com>
2024-10-09 17:27:27 -0700

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,16 +2,19 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div id="about" className="h-screen scroll-mt-32">
-      <div className="section container mx-auto py-40z mt-32">
-        <div className="columns-2">
+    <div id="about" className="h-screen mt-32 scroll-mt-32">
+      <div className="grid grid-rows-3 grid-flow-col gap-4">
+        <div className="row-span-2 w-1/3">
           <Image
             src="/images/main/ben-milner.jpg"
             width={500}
             height={500}
             alt="Ben Milner"
-            className="gap-8 w-1/2 rounded-lg bg-gradient-to-br from-teal-400 to-blue-400 p-1 shadow-xl shadow-slate-900"
+            className="rounded-full bg-gradient-to-br from-teal-400 to-blue-400 p-1 shadow-xl shadow-slate-900"
           />
+        </div>
+        <div className="row-span-2 col-span-2">
+          <p>TEST</p>
         </div>
       </div>
     </div>
